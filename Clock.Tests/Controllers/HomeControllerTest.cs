4d1b818fbeb4e71469441fe8107a6e7ca2ce3aea
wellysanusi <91016534+wellysanusi@users.noreply.github.com>
2021-09-20 08:15:38 +0800

    Add files via upload

---
+++ Clock.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Clock.Controllers;
+
+namespace Clock.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            HomeController controller = new HomeController();
+            ViewResult result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ClockTick()
+        {
+            HomeController controller = new HomeController();
+            JsonResult result = controller.ClockTick() as JsonResult;
+            Assert.IsNotNull(result);
+        }
+    }
+}
